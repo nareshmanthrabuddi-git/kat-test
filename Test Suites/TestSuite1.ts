a363ae8d84da4a6e6740fa3905e443fd591d440f
naresh.manthrabuddi <naresh.manthrabuddi@BRILAP2530.BRILLIO.COM>
2021-06-28 20:37:03 +0530

    modified

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>88324241-f178-4574-991d-665a9f904cad</testSuiteGuid>
+   <testCaseLink>
+      <guid>42a77596-3a3e-4217-bbbe-f1764421faac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
